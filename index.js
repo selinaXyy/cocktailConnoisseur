608bbaf8ae965c81c7f6c6078215e12ae082f6fc
Selina Xue <selinaxue@selinas.home>
2024-03-23 02:35:14 -0700

    run auto-formatter

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.get("/",(req,res)=>{
     res.render("index.ejs");
 })
 
-app.post("/",async (req,res)=>{
+app.post("/", async (req,res)=>{
     const queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?";
-    const userChosenType= req.body.type.value;
-    const userChosenFilter = req.body.filter.value;
+    const userChosenType= req.body.type;
+    const userChosenFilter = req.body.filter;
     let APIrequest = "";
     
     if (userChosenType == "cocktailName"){
@@ -30,40 +30,59 @@ app.post("/",async (req,res)=>{
 
     try {
         const response = await axios.get(APIrequest);
-        const drink = response.data.drinks[0];
-        const drinkName = drink.strDrink;
-        const drinkInstructions = drink.strInstructions;
-        const cocktailImg = drink.strDrinkThumb;
-        let ingredientArr = new Array();
-        let ingredientMeasurementArr = new Array();
-        
-        //query ingredient
-        let i = 1;
-        while (drink[`strIngredient${i}`] != null){
-            ingredientArr.push(drink[`strIngredient${i}`]);
-            i++;
-        }
-
-        //query measurement
-        let j = 1;
-        while (drink[`strMeasure${j}`] != null){
-            ingredientMeasurementArr.push(drink[`strMeasure${j}`]);
-            j++;
-        }
-
-        res.render("index.ejs",{
-            drinkName,
-            drinkInstructions,
-            cocktailImg,
-            ingredientArr,
-            ingredientMeasurementArr
-        });
-        
-      } catch (error) {
-        console.log(error.response.data);
-        res.status(500);
-      }
+        if (userChosenType == "cocktailIngredient"){
+            const ingredient = response.data.ingredients[0];
+            const ingredientName = ingredient.strIngredient;
+            const ingredientDescription = ingredient.strDescription;
     
+            res.render("index.ejs", {
+                ingredientName,
+                ingredientDescription,
+            });
+
+        }
+        else{
+            const drink = response.data.drinks[0];
+            const drinkName = drink.strDrink;
+            const drinkInstructions = drink.strInstructions;
+            const cocktailImg = drink.strDrinkThumb;
+            let ingredientArr = new Array();
+            let ingredientMeasurementArr = new Array();
+                
+            //query ingredient
+            let i = 1;
+            while (drink[`strIngredient${i}`] != null){
+                ingredientArr.push(drink[`strIngredient${i}`]);
+                i++;
+            }
+        
+            //query measurement
+            let j = 1;
+            while (drink[`strMeasure${j}`] != null){
+                ingredientMeasurementArr.push(drink[`strMeasure${j}`]);
+                j++;
+            }
+        
+            res.render("index.ejs",{
+                drinkName,
+                drinkInstructions,
+                cocktailImg,
+                ingredientArr,
+                ingredientMeasurementArr,
+            });
+        }    
+      } 
+      catch (error){
+        if (error instanceof TypeError){
+            res.render("index.ejs", {
+                error: `"${userChosenFilter}" not found`,
+            });
+        }
+        else{
+            console.log(error);
+            res.status(500);
+        }
+      }
 });
 
 app.listen(port, () =>{
